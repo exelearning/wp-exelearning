1dec38084847de837e282ec28269e2a7f88434a8
erseco <info@ernesto.es>
2026-08-04 13:00:22 +0200

    Remove two JS files that were never enqueued
    assets/js/exelearning.js and assets/js/exelearning-admin.js each held a
    single console.log inside a jQuery IIFE. No wp_enqueue_script or
    wp_register_script call references either path -- every enqueue in the
    plugin names its asset literally -- so neither file has ever reached a
    browser.
    
    They were still measured, and reported as two uncovered files.

--- assets/js/exelearning-admin.js
+++
@@ -1,6 +0,0 @@
-(function($) {
-    'use strict';
-
-    console.log("exelearning-admin.js loaded")    
-
-})(jQuery);
--- assets/js/exelearning.js
+++
@@ -1,6 +0,0 @@
-(function($) {
-    'use strict';
-
-    console.log("exelearning.js loaded")    
-
-})(jQuery);
